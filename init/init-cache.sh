62a7c96b09e0e75b3c95293bc567ec5af8b50f27
Vincent Palita <vincent.palita@whoz.com>
2025-02-06 22:39:02 +0100

    add initial actions

--- init/init-cache.sh
+++ init/init-cache.sh
@@ -2,16 +2,9 @@
 
 WORKING_DIRECTORY=$1
 echo "WORKING_DIRECTORY:" $WORKING_DIRECTORY
-BRANCHES_CACHE_FOLDER=/mnt/github/actions-runner/_work/data/branches
-FRAMEWORKS_CACHE_FOLDER=/mnt/github/actions-runner/_work/data/cache
-GIT_STRATEGY=none # we do the checkout process only once in init stage
-INIT_REPOSITORY_PIPELINE_ID_ENV_FILE=tmp.init-repository-pipeline-id.env
-MASTER_BRANCH=master
-NX_MAX_WORKERS=4
-NX_SKIP_LOG_GROUPING=false
-NX_TASKS_RUNNER_DYNAMIC_OUTPUT=false
-NX_PLUGIN_NO_TIMEOUTS=${CI_NX_PLUGIN_NO_TIMEOUTS}
-NX_VERBOSE_LOGGING=${CI_NX_VERBOSE_LOGGING}
+
+BRANCHES_CACHE_FOLDER=${WORKING_DIRECTORY_PREFIX}/branches
+FRAMEWORKS_CACHE_FOLDER=${WORKING_DIRECTORY_PREFIX}/cache
 
 function getSlug {
   local name=$1
@@ -91,12 +84,10 @@ function initializeFrameworkCache {
   if [ $framework = ".nx" ]; then
     cd $folderWithWorkingDirectory
     pwd
-    echo "NX_RESET" $NX_RESET
     if [ "$NX_RESET" == "true" ]; then
       echo "nx reset cache requested"
       yarn nx reset
     fi
-    yarn nx graph --verbose --file=$folderWithWorkingDirectory/.nx/workspace-data/project-graph.json
   fi
 }
 
